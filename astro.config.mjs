81d8bbd1d7038e9d24c5434e1037e40fc0626128
Hasan Sarwer <hasanmathju@gmail.com>
2025-07-05 20:37:26 +0600

    RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://violet-virgo-3b1zr1eet-hasansarwers-projects.vercel.app"
+    
+});
