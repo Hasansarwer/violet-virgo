0227ecc89a953aa2e2369d28dfe6234bdf815265
Hasan Sarwer <hasanmathju@gmail.com>
2025-07-05 21:00:48 +0600

    components with PREACT

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://violet-virgo-3b1zr1eet-hasansarwers-projects.vercel.app"
-    
+  site: "https://violet-virgo-3b1zr1eet-hasansarwers-projects.vercel.app",
+  integrations: [preact()]
 });
